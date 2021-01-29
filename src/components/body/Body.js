e49bdf38368588ee0b849135db09917a0c8275d2
jakedoren <74628634+jakedoren@users.noreply.github.com>
2021-01-28 21:31:16 -0500

    added animation

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Body.css'
 import image from '../../images/codepic.jpeg'
-
+import { init } from 'ityped'
 export default function Body() {
+
+    useEffect(() => {
+        const myElement = document.querySelector('#myElement')
+        init(myElement, { showCursor: false, strings: ['Jacob Doren', 'a developer', 'a designer']})
+    }, []) 
+
     return (
         <div className="img-wrapper">
-            <h1 className="brand"><span className="hi">Hi! I'm</span> Jacob Doren</h1>
+            <h1 className="brand">
+               Hi i'm <span id='myElement'></span>
+            </h1>
             <img src={image} alt="codeImg" className="body-image" />
         </div>
     )
