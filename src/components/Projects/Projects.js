15e06a01adbe867aeaf7ff1df30b5ca1123c8543
Jacob Doren <Jacob Doren>
2021-04-03 00:57:20 -0400

    added new projects and updated responsiveness

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Projects.css'
 import rickapp2 from '../../images/rickapp3.jpg'
 import arcadeapp from '../../images/arcadelux.jpg'
+import apiapp from '../../images/apiapp.jpg'
 
 import rickapp from '../../images/rickapp.jpg'
 import todoapp from '../../images/todoapp.jpg'
@@ -17,6 +18,9 @@ export default function Projects() {
             <div className="project-container">
                 <div className="project-left box">
                     <a href="https://sharp-blackwell-56aa0f.netlify.app" target="_blank" rel="noreferrer"><img src={rickapp2} className="rickapp" alt="rickandmortyapp"/></a>
+                </div>
+                <div className="project-mid box">
+                    <a href="https://compassionate-tesla-e7e979.netlify.app/" target="_blank" rel="noreferrer"><img className="api-app" src={apiapp} alt="apiapp" /></a>
                 </div>
                 <div className="project-right box ">
                     <a href="https://happy-pike-9aa462.netlify.app/" target="_blank" rel="noreferrer"><img src={arcadeapp} className="todo-app" alt="TodoApp"/></a>
