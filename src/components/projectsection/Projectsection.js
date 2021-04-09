e412cb0b4a8c9a3f74176d22d0726c2f42f5944e
Jacob Doren <jakedoren@gmail.com>
2021-04-08 21:23:46 -0400

    responsive changes

--- src/components/projectsection/Projectsection.js
+++ src/components/projectsection/Projectsection.js
@@ -11,10 +11,10 @@ const projectsection = () => {
         <div className="project-section" name="project-section">
             <h1 className="proj-title">Projects</h1>
             <div className="project-section-wrap">
-            <Projects title="Character API" image={apiapp} link="https://compassionate-tesla-e7e979.netlify.app/"/>
-            <Projects title="Employee Sort" image={employeesort} link="https://stoic-austin-50c836.netlify.app/" />
-            <Projects title="Character Generator" image={rickapp3} link="https://sharp-blackwell-56aa0f.netlify.app/" />
-            <Projects title="Arcadelux" image={Arcadelux} link="https://happy-pike-9aa462.netlify.app"/>
+                <Projects title="Character API" image={apiapp} link="https://compassionate-tesla-e7e979.netlify.app/"/>
+                <Projects title="Employee Sort" image={employeesort} link="https://stoic-austin-50c836.netlify.app/" />
+                <Projects title="Character Generator" image={rickapp3} link="https://sharp-blackwell-56aa0f.netlify.app/" />
+                <Projects title="Arcadelux" image={Arcadelux} link="https://happy-pike-9aa462.netlify.app"/>
             </div>
         </div>
     )
